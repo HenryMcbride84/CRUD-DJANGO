9da82fb4ae752284f499c60f3a709643a8b5767a
HenryMcbride <saveusall1984@gmail.com>
2026-03-30 22:51:34 -0600

    (feat)Backend Se Agregar modelos a la app en DJango,variables de entorno,modificaciones en settings apartado de DATABASES

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+""" 
+CREATE TABLE rol (
+	id_rol serial PRIMARY KEY,
+	nombre varchar(50),
+	descripcion varchar(255),
+	status boolean 
+	); """
+class Rol(models.Model): 
+    id_rol = models.IntegerField(primary_key=True)
+    nombre = models.CharField(max_length=50, blank=True,null=True)
+    descripcion= models.CharField(max_length=255,blank=True,null=True) 
+    status= models.BooleanField(blank=True,null=True)
+
+    class Meta:
+        managed = False
+        db_table= 'rol'    
+
+
+class Users(models.Model):
+    id_user = models.IntegerField(primary_key=True)
+    username = models.CharField(max_length=50, blank=True,null=True)
+    password= models.CharField(max_length=255)
+    id_rol = models.IntegerField
+    models.ForeignKey(Rol, null=True, on_delete=models.CASCADE,db_column='id_rol')
+
+    class Meta:
+         db_table='users'
